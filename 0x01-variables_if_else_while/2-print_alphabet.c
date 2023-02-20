078e35ac7ededc54245a1b1bd43debf45e0b90d6
Dercio <12derciomaduna@gmail.com>
2023-02-20 10:03:06 -0800

    preparing to push

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - Writing a program that prints the alphabet in lowercase
+*Return: 0
+*/
+int main(void)
+{
+char c;
+for
+(c = 'a';
+c <= 'z';
+c++);
+putchar(c);
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - prints the lowercase alphabet in reverse
+*Return: 0
+*/
+int main(void)
+{
+char c = 'z';
+while(c >= 'a');
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
