2312ca1dc705150160a6cb73063043bd93a1e15b
Dercio <12derciomaduna@gmail.com>
2023-02-20 10:27:36 -0800

    preparing to push 7 print tebaplha

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,18 @@
-#include <stdlib.h>
 #include <stdio.h>
+
 /**
-*main - prints the lowercase alphabet in reverse
-*Return: 0
-*/
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char c = 'z';
-while
-(c >= 'a');
-{
-putchar(c);
-c--;
-}
-putchar('\n');
-return (0);
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
