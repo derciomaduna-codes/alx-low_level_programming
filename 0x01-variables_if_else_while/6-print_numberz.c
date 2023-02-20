0d15a59ee24d1318ce7da35b0989f8ead6f6f5f9
Dercio <12derciomaduna@gmail.com>
2023-02-20 10:24:57 -0800

    preparing to push 6-numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
+
 /**
-*main - Prints all single digit numbers of base 10 starting from 0,
-*Return: 0
-*/
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int a;
-for
-(a = 0;
-a <= 9;
-a++);
-{
-putchar(a);
-}
-putchar('\n');
-return (0);
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
 }
