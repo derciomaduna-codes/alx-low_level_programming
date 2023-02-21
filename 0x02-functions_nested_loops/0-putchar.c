2ed3ca3fc7a9502cf4ab9dfcfedd69c0243cbe66
Dercio <12derciomaduna@gmail.com>
2023-02-21 07:40:04 -0800

    pushing put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Entry Point
+*Description - 'Print putchar and a new line.'
+*Return: 0 (Success)
+*/
+
+int main(void)
+{
+printf("_putchar");
+printf("\n");
+return (0);
+}
