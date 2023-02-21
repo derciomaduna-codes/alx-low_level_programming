b68f2dd0e48ce5097299219727fc6a43c00eef20
Dercio <12derciomaduna@gmail.com>
2023-02-21 08:42:24 -0800

    pushing print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,22 +2,19 @@
 
 /**
  * main - check the code
- * Description : 'prints all the alphabets'
+ * Description - 'prints all the alphabets'
  * Return: Always 0.
  */
 int main(void)
 {
-    print_alphabet();
-    return (0);
+print_alphabet();
+return (0);
 }
 
-void print_alphabet(void){
-    // Get the character to be written
-    char ch = 'a';
-  
-    // Write the Character to stdout
-    for (ch = 'a'; ch <= 'z'; ch++)
-        putchar(ch);
-  
-    return (0);
+void print_alphabet(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+return (0);
 }
