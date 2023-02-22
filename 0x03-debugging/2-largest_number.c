74411fa297e159bad26dc0c2d10a86f2e7256a4b
Dercio <12derciomaduna@gmail.com>
2023-02-22 00:48:30 -0800

    puhing to test 1 and 2

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+int largest;
+
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > a && b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+
+return (largest);
+}
