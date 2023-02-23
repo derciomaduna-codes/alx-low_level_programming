54069591ea0cb05880640365183b2accca3b0c40
Dercio <12derciomaduna@gmail.com>
2023-02-23 05:39:12 -0800

    testing is multiplication

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers-prints the numbers, from 0 to 9, followed by a new line.
+ * Return: 0 (Success)
+ */
+
+void print_numbers(void)
+{
+int k;
+for (k = 0; k < 10; k++)
+{
+printf("%d", k);
+}
+printf("\n");
+}
