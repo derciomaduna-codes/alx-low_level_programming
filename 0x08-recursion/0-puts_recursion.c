61d96227adb6bdaf24f7f8c558ac146fd637ca9a
Dercio <12derciomaduna@gmail.com>
2023-03-08 22:18:49 -0800

    trsting task 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _void _puts_recursion - fill a block of memory with a specific value
+ * @s: string to print
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s){
+printf("%s",*s);
+}
