b2d7058a45fb098b7574699e35f451f365deffd4
Dercio <12derciomaduna@gmail.com>
2023-03-10 04:19:02 -0800

    testing task two

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * main - Print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
- * Return: Always 0 (Success)                                          
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
